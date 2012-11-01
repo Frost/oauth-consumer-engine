eb948428eefc439da961880b88674b73ddd72089
Robert Johansson <johansson@studentcompetitions.com>
2012-11-01 13:53:30 +0100

    Added sending of the initial_page parameter

--- lib/engine_id.rb
+++ lib/engine_id.rb
@@ -10,6 +10,15 @@ module OmniAuth
       info { raw_info['info'] }
       extra { {'admin' => raw_info['admin'], 'raw_info' => raw_info} }
 
+
+      # Overrides the default request_phase method of the OAuth2 strategy
+      # Adds a extra parameter called initial_page sent to the OAuth provider, this is specific for the studentcompetition login provider
+      def request_phase
+        # Allowed values for the initial_page param is sign_up or sign_in or no value
+        initial_page = @request.params[:initial_page] || "sign_in"
+        redirect client.auth_code.authorize_url({:redirect_uri => callback_url, :initial_page => initial_page}.merge(authorize_params))
+      end
+
       def raw_info
         @raw_info ||= access_token.get("/auth/engine_id/user.json?oauth_token=#{access_token.token}").parsed
       end
--- lib/oauth_consumer_engine/version.rb
+++ lib/oauth_consumer_engine/version.rb
@@ -1,3 +1,3 @@
 module OauthConsumerEngine
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
