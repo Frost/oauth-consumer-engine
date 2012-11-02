bb67595022ac3c891f20280d446edf599934d7d6
Robert Johansson <johansson@studentcompetitions.com>
2012-11-02 14:07:45 +0100

    Added provider_ignores_state setting since the state param was not sent from the SC-API login portal

--- lib/engine_id.rb
+++ lib/engine_id.rb
@@ -5,6 +5,8 @@ module OmniAuth
     class EngineId < OmniAuth::Strategies::OAuth2
       option :name, "engine_id"
       option :client_options, { :site => OauthConsumerEngine.config.custom_provider_url }
+      # Got CSRF error without this when calling the SC login portal
+      option :provider_ignores_state, true
 
       uid { raw_info['uid'] }
       info { raw_info['info'] }
@@ -14,7 +16,7 @@ module OmniAuth
       # Overrides the default request_phase method of the OAuth2 strategy
       # Adds a extra parameter called initial_page sent to the OAuth provider, this is specific for the studentcompetition login provider
       def request_phase
-        # Allowed values for the initial_page param is sign_up or sign_in or no value
+        # For allowed values for the initial_page param, check the SC-API (The CustomFailure class)
         initial_page = @request.params[:initial_page] || "sign_in"
         redirect client.auth_code.authorize_url({:redirect_uri => callback_url, :initial_page => initial_page}.merge(authorize_params))
       end
